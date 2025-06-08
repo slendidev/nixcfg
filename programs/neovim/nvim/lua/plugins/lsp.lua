8fca7b0f03ee0d373a56edbe47c76c5745ee89c2
Slendi <slendi@socopon.com>
2025-06-08 05:52:34 +0300

    complete
    Signed-off-by: Slendi <slendi@socopon.com>

--- programs/neovim/nvim/lua/plugins/lsp.lua
+++ programs/neovim/nvim/lua/plugins/lsp.lua
@@ -1,69 +1,7 @@
 return {
 	{
 		'neovim/nvim-lspconfig',
-		dependencies = {
-			--'saghen/blink.cmp',
-			'ms-jpq/coq_nvim',
-			dependencies = 'rafamadriz/friendly-snippets',
-			version = '*',
-
-			config = function(_, opts)
-				vim.g.coq_settings = {
-					keymap = {
-						recommended = false,
-					},
-					auto_start = true,
-				}
-
-				local opts = { expr = true, silent = true }
-
-				-- exit to normal, even if menu open
-				vim.api.nvim_set_keymap('i', '<Esc>',
-					[[pumvisible() ? "\<C-e><Esc>" : "\<Esc>"]],
-					opts)
-
-				-- abort completion
-				vim.api.nvim_set_keymap('i', '<C-c>',
-					[[pumvisible() ? "\<C-e><C-c>" : "\<C-c>"]],
-					opts)
-
-				-- backspace always
-				vim.api.nvim_set_keymap('i', '<BS>',
-					[[pumvisible() ? "\<C-e><BS>"  : "\<BS>"]],
-					opts)
-
-				-- delete word before cursor
-				vim.api.nvim_set_keymap('i', '<C-w>',
-					[[pumvisible() ? "\<C-e><C-w>" : "\<C-w>"]],
-					opts)
-
-				-- delete all before cursor
-				vim.api.nvim_set_keymap('i', '<C-u>',
-					[[pumvisible() ? "\<C-e><C-u>" : "\<C-u>"]],
-					opts)
-
-				-- <CR> selects if menu open, else normal enter
-				vim.api.nvim_set_keymap('i', '<C-CR>',
-					[[pumvisible() 
-					and (vim.fn.complete_info().selected == -1 
-					and "\<C-e><CR>" 
-					or "\<C-y>") 
-					or "\<CR>"]],
-					opts)
-
-				-- tab/shift-tab to navigate menu
-				vim.api.nvim_set_keymap('i', '<Tab>',
-					[[pumvisible() ? "\<C-n>" : "\<Tab>"]],
-					opts)
-				vim.api.nvim_set_keymap('i', '<S-Tab>',
-					[[pumvisible() ? "\<C-p>" : "\<BS>"]],
-					opts)
-
-				require'coq'
-			end,
-		},
 		config = function()
-			local coq = require'coq'
 
 			vim.api.nvim_create_autocmd('BufWritePre', {
 				command = 'lua vim.lsp.buf.format()',
@@ -72,13 +10,16 @@ return {
 
 			local lspconfig = require 'lspconfig'
 			local function config(_config)
-				local cap = coq.lsp_ensure_capabilities(vim.lsp.protocol.make_client_capabilities())
+				local cap = vim.lsp.protocol.make_client_capabilities()
+				vim.o.completeopt = 'menuone,noselect'
 				return vim.tbl_deep_extend('force', {
-					completion = {
-						keyword_length = 0,
-						autocomplete = false
-					},
 					on_attach = function(client, buffer)
+						vim.bo[buffer].omnifunc = 'v:lua.vim.lsp.omnifunc'
+						vim.lsp.completion.enable(true, client.id, buffer, { autotrigger = true })
+						vim.keymap.set('i', '<C-j>', 'pumvisible() ? "\\<C-n>" : "\\<C-j>"', { expr = true, noremap = true, silent = true })
+						vim.keymap.set('i', '<C-k>', 'pumvisible() ? "\\<C-p>" : "\\<C-k>"', { expr = true, noremap = true, silent = true })
+						vim.keymap.set('i', '<C-CR>', 'pumvisible() ? "\\<C-y>" : "\\<CR>"', { expr = true, noremap = true, silent = true })
+
 						vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, { silent = true })
 						vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, { silent = true })
 						vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, { silent = true })
